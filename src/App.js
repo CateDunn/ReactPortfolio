41892407e3958c8e3dda8834e784b1e3afa45725
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-07-15 17:41:19 -0400

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 class App extends Component {
@@ -48,17 +47,16 @@ class App extends Component {
 
   render() {
     return (
-      // <Router>
+      
       <div className="App">
         <Header data={this.state.resumeData.main}/>
-        {/* <Route exact path="/" component={Header} /> */}
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
-      // </Router>
+     
     );
   }
 }
