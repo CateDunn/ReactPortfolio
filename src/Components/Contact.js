6719ea246f0509e754226280e247ce9a1c00cfc0
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-07-28 12:41:40 -0400

    new resume, updating About me and bio, removing contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,11 +5,11 @@ class Contact extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
+      var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -34,7 +34,7 @@ class Contact extends Component {
 
          <div className="row">
             <div className="eight columns">
-
+{/* 
                <form action="" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
@@ -65,7 +65,7 @@ class Contact extends Component {
                      </span>
                   </div>
 					</fieldset>
-				   </form>
+				   </form> */}
 
            <div id="message-warning"> Error boy</div>
 				   <div id="message-success">
@@ -77,11 +77,11 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Contact Information</h4>
 					   <p className="address">
 						   {name}<br />
-						   {street} <br />
 						   {city}, {state} {zip}<br />
+                     {email}<br />
 						   <span>{phone}</span>
 					   </p>
 				   </div>
